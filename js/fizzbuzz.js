7fc83aa75808b27e13fc6e0c2119fbbf5f699146
RMKendall <ryan.morris.kendall@gmail.com>
2020-09-17 09:52:00 -0500

    Update saves for class morning pushup exercises
    - variation on FizzBuzz
    - ES5 warmup for prep going into ES6

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -5,29 +5,29 @@ for(i = 1; i <= 100; i++){
     if (i % 3 != 0 && i % 5 != 0) console.log(i)
 }
 
-let count = 1;
-let interval = 2000;
-let intervalID = setInterval(myFunc, interval);
-function myFunc(){
-    if(count <= 5){
-        alert("Some");
-        count++;
-    }
-}
+// let count = 1;
+// let interval = 2000;
+// let intervalID = setInterval(myFunc, interval);
+// function myFunc(){
+//     if(count <= 5){
+//         alert("Some");
+//         count++;
+//     }
+// }
 
 
-var delay = 5000; // delay time in milliseconds
-var timeoutId = setTimeout(function () {
-alert('Here is a delayed hello!');
-}, delay);
-// to cancel the timeout, you can call
+// var delay = 5000; // delay time in milliseconds
+// var timeoutId = setTimeout(function () {
+// alert('Here is a delayed hello!');
+// }, delay);
+// // to cancel the timeout, you can call
 // clearTimeout(timeoutId);
-// prior to the delay expiring
-var delay2 = 6000;
-var to2 = setTimeout(function () {
-clearTimeout(timeoutId);
-alert('Stopped alerts!');
-}, delay2);
+// // prior to the delay expiring
+// var delay2 = 6000;
+// var to2 = setTimeout(function () {
+// clearTimeout(timeoutId);
+// alert('Stopped alerts!');
+// }, delay2);
 
 function fizzBuzz(num){
     for(let i = 1; i <= num; i++){
